d5ff1a646e794c32a45296a190ed9200deeb6c3b
liming-ds3 <liming-ds3@gome.com.cn>
2020-04-22 18:18:46 +0800

    添加详情

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -1,9 +1,29 @@
 <template>
   <section>
     <Header></Header>
-    <div style="height: 700px;">
+    <div>
+      <div class="content-all">
+        <div class="content-img">
+          <div class="content-list">
+            <ul class="content-ul">
+              <li>
+                <router-link :to="{name:'Detail',params:{type: 'qyjs'}}">企业介绍</router-link>
+              </li>
+              <li>
+                <router-link :to="{name:'Detail',params:{type: 'fzlc'}}">发展历程</router-link>
+              </li>
+              <li>
+                <router-link :to="{name:'Detail',params:{type: 'qyry'}}">企业荣誉</router-link>
+              </li>
+              <li>
+                <router-link :to="{name:'Detail',params:{type: 'qywh'}}">企业文化</router-link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
       <div v-if="qyjs">
-        企业介绍
+        <Qyjs></Qyjs>
       </div>
       <div v-if="fzlc">
         发展历程
@@ -23,6 +43,7 @@
 // @ is an alias to /src
 import Header from "@/components/Header.vue";
 import Footer from "@/components/Footer";
+import Qyjs from "./detail/qyjs.vue";
 export default {
   name: "Home",
   data () {
@@ -62,7 +83,38 @@ export default {
   },
   components: {
     Header,
-    Footer
+    Footer,
+    Qyjs
   }
 };
 </script>
+<style lang="stylus" scoped="">
+.content-all
+  height: 280px;
+  background: url(../assets/img/qyjs.jpg) center no-repeat;
+.content-img
+  margin: 0 100px;
+.content-list
+  position: relative;
+  left: 0;
+  top: 215px;
+  background: #f8f8f8;
+  height: 65px;
+  width: 100%;
+.content-ul
+  li
+    display: inline-block;
+    text-align: center;
+    height: 66px;
+    line-height: 66px;
+    font-size: 14px;
+    font-weight: bold;
+    float: left;
+    width: 132px;
+    a
+      color: black;
+      cursor: pointer;
+      text-decoration: none;
+      &:hover
+        color: #efaa00;
+</style>
---
+++ src/views/detail/qyjs.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="detail content">
+    <div>
+      <div class="detail-title">江苏中烁</div>
+      <div>江苏中烁热能科技有限公司，成立于2020年3月，公司位 于江苏省南京市溧水区，一家专业从事工业燃烧(加热) 系统设备及配件的工程技术公司</div>
+    </div>
+    <div>
+    </div>
+  </section>
+</template>
+<script>
+export default {}
+</script>
+<style lang="stylus" scoped="">
+.detail
+  display: flex;
+  margin-bottom: 20px;
+  height: 500px;
+  > div:first-child
+    flex: 1;
+    > div:first-child
+      margin-bottom: 20px;
+    > div:last-child
+      line-height: 22px;
+    // background: green;
+  > div:last-child
+    width: 920px;
+    background: url('../../assets/img/qyjs1.jpg') center no-repeat;
+    background-size:cover;
+</style>
